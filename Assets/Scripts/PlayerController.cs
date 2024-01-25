a155fd1f1f0217e4a2a8fb9047543194467e8518
quezadra <rmq13@scarletmail.rutgers.edu>
2024-01-24 21:01:13 -0500

    jan 24- 9pm
    ui text you win and count

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,25 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
 
+        SetCountText();
+        winTextObject.SetActive(false);
     }
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+
+        countText.text = " Count: " + count.ToString();
+        if(count>= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate()
@@ -33,11 +52,13 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        
+
         if (other.gameObject.CompareTag("PickUp"))
         {
-            
             other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText() ;
         }
     }
 }
